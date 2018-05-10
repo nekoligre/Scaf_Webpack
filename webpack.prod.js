223e1ce30cf15782f6ac5cc3dc423ce7889b07fb
alberto gonzalez <alberto.gonzalez@gft.com>
2018-05-10 12:54:26 +0200

    feat(prod): añadimos config de prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,39 @@
 const path = require('path');
 const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src/app.module.js'),
   mode: 'production',
   output: {
     filename: '[name].bundle.js',
-    devtoolLineToLine: true,
-    pathinfo: true,
-    sourceMapFilename: '[name].bundle.js.map'
+    path: path.resolve(__dirname, 'build')
+
   },
   module: {
-    rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.pug$/, loader: 'pug-loader', include: path.join(__dirname, '/src') },
-      { test: /\.sass$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
+    rules: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        include: path.join(__dirname, '/src')
+      },
+      {
+        test: /\.sass$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
     ]
   },
-  devServer: {
-    publicPath: '/',
-    contentBase: path.join(__dirname, '/demo'),
-    compress: true
-  },
-  devtool: 'source-map'
+  plugins: [
+    new CleanWebpackPlugin(['build'],{ verbose: true}),
+    new HtmlWebpackPlugin({
+      hash:true,
+      minify:true,
+      showErrors:true
+    })
+  ]
 };
