88086d87372468cf7825d0a5d9c28e3aee441448
alberto gonzalez <alberto.gonzalez@gft.com>
2018-05-22 22:32:21 +0200

    fix(prod): añadimos configuracion produccion

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,36 +4,25 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, 'src/app.module.js'),
-  mode: 'production',
+  entry: {
+    app: ['./src/vendor/angular.js', './src/app.module.js']
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'build')
 
   },
   module: {
-    rules: [{
-        test: /\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader',
-        include: path.join(__dirname, '/src')
-      },
-      {
-        test: /\.sass$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
-      }
+    rules:  [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.pug$/, loaders: ['html-loader', 'pug-html-loader'] },
+      { test: /\.sass$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] }
     ]
   },
   plugins: [
-    new CleanWebpackPlugin(['build'],{ verbose: true}),
+    new CleanWebpackPlugin(['build'], {}),
     new HtmlWebpackPlugin({
-      hash:true,
-      minify:true,
-      showErrors:true
+      template: 'src/index.html'
     })
   ]
 };
