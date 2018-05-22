861c0efee9304605d4ee59f4304233faa57ed150
Ju4nPe <juama31@gmail.com>
2018-05-22 23:57:30 +0200

    feat(controller): añade ejemplo de llamada a la api para comprobar el funcionamiento

--- src/components/myComponent.controller.js
+++ src/components/myComponent.controller.js
@@ -1,5 +1,12 @@
-let myControllerCtrl = ($scope) => {
+let myControllerCtrl = ($scope, $http) => {
     $scope.img = require('./img.png');
+    
+    $http({
+        method: 'GET',
+        url: 'http://localhost:7000/api/hola'
+    }).then(response => {
+        $scope.hello = response.data.label;
+    })
 };
 
-export default angular.module('controllers.myComponent', []).controller('myControllerCtrl',['$scope', myControllerCtrl]).name
+export default angular.module('controllers.myComponent', []).controller('myControllerCtrl',['$scope', '$http', myControllerCtrl]).name
